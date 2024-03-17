80e423d3b6aa1286b10fca6b6e2bcbfbbdc9fbd9
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2024-03-17 02:52:50 +0100

    dburi update

--- config/production.ts
+++ config/production.ts
@@ -1,5 +1,5 @@
 export default {
     port:4000,
-    dbUri:'mongodb+srv://iruleadeleye:modelite@cluster0.vcwv0ly.mongodb.net/',
+    dbUri:'mongodb+srv://iruleadeleye:modelite@cluster0.vcwv0ly.mongodb.net/shortURL',
     corsOrigin:'https://scissor-ui.vercel.app' 
 }
